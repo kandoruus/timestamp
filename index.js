cc4be8efd7475271fd10a16dd87a6c2e2691885b
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-10-19 16:31:25 -0400

    sss

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ const e = require("express");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
